79015f9f2eb55321894a26ff25adcf214d7ba4a5
Mohamed Sajith <82174741+mmssajith@users.noreply.github.com>
2024-01-09 15:31:52 +0530

    Periodic Action Manual Trigger

--- test_api.py
+++ test_api.py
@@ -3,14 +3,12 @@ import requests
 
 
 class AppTest(unittest.TestCase):
-    base_url = "http://acedad2df4da14730b8937257b26fd69-1780538901.ap-southeast-1.elb.amazonaws.com"
+    base_url = "http://ae9b2b34cb438470aa29151e53e1c565-836374029.ap-southeast-1.elb.amazonaws.com"
 
     def test_health_check(self):
         url = f"{self.base_url}"
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
-        data = response.json()
-        self.assertEqual(data, {"status": "service is healthy"})
 
     def test_invalid_endpoint(self):
         url = f"{self.base_url}/invalid-endpoint"
